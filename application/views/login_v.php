2d196f60273ac86d436604cf18d16f8915bfe805
Secret <agungramadhani684@gmail.com>
2023-09-03 12:50:43 +0800

    update footer

--- application/views/login_v.php
+++ application/views/login_v.php
@@ -13,23 +13,26 @@
               <div class="p-5">
                 <div class="text-center">
                   <h1 class="h4 text-gray-900 mb-4">Login</h1>
-                  <br><center><p>Repost by <a href='https://stokcoding.com/' title='StokCoding.com' target='_blank'>StokCoding.com</a></p></center>
-                  
+                  <br>
+                  <center>
+                    <p>Repost by <a>Agung Ramadhani</a></p>
+                  </center>
+
                 </div>
 
-                <?= validation_errors('<div class="alert alert-danger alert-dismissible fade show" role="alert">','<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <?= validation_errors('<div class="alert alert-danger alert-dismissible fade show" role="alert">', '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 </div>') ?>
 
                 <?= $this->session->flashdata('msg'); ?>
-                
+
                 <?= form_open('Auth/LoginController', 'class="user"'); ?>
-                  <div class="form-group">
-                    <input type="text" class="form-control form-control-user" id="username" name="username" placeholder="Username" autofocus value="<?= set_value('username') ?>">
-                  </div>
-                  <div class="form-group">
-                    <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
-                  </div>
-                  <button type="submit" class="btn btn-primary btn-user btn-block">Login</button>
+                <div class="form-group">
+                  <input type="text" class="form-control form-control-user" id="username" name="username" placeholder="Username" autofocus value="<?= set_value('username') ?>">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-primary btn-user btn-block">Login</button>
                 </form>
                 <hr>
                 <div class="text-center">
